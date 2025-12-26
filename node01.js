03eed569a111f5224044a4d5200502a66ca58b09
Mario Metshein <metshein@gmail.com>
2025-12-27 01:49:58 +0200

    Add initial JavaScript code for greeting functionality

---
+++ node01.js
@@ -0,0 +1,13 @@
+
+//Tavaline javaskript kood
+let nimi = "maailm";
+console.log("Tere, " + nimi + "!");
+
+let tervitus = (nimi) => {
+    return "Tere, " + nimi + "!";
+}
+
+console.log(tervitus("Eesti"));
+console.log(tervitus(nimi));
+
+// Globaalsed objektid
