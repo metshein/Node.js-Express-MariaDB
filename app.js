aa771c54972a499c0eb18188993d9006a5c91e41
Mario Metshein <metshein@gmail.com>
2026-01-02 00:30:12 +0200

    Refactor routes to use EJS rendering and update index page content

--- app.js
+++ app.js
@@ -6,20 +6,19 @@ app.set('view engine', 'ejs')
 
 //html failide teenindamine
 app.get('/', (req, res) => {
-  res.sendFile('./views/index.html', { root: __dirname })
+  res.render('index')
 })
-app.get('/teenused', (req, res) => {
-  res.sendFile('./views/teenused.html', { root: __dirname })
+app.get('/meist', (req, res) => {
+  res.render('meist')
 })
 
-//ümber suunamine
-app.get('/vana-leht', (req, res) => {
-  res.redirect('/')
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt')
 })
 
 //404 lehe teenindamine
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
 
 
